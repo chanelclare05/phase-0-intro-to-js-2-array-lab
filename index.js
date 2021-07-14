6d13fe40a55f564cdcd879c33977e9c187eedf64
Chanel Donahoe <cdonahoe05@gmail.com>
2021-07-14 23:53:29 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(){
+    let members = [...cats, "Broom"];
+    return members;
+}
+
+function prependCat(){
+    let members = ['Arnold',...cats];
+    return members;
+}
+
+function removeLastCat(){
+    return cats.slice(0,2);
+}
+
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
